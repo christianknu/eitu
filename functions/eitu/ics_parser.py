a50cff706d087f24c648acb888fd78299a9db243
Bjørn Friese <bearfrieze@gmail.com>
2016-04-06 18:53:31 +0200

    Fixed bugs in ics parser and corrected list of fakes

--- functions/eitu/ics_parser.py
+++ functions/eitu/ics_parser.py
@@ -2,21 +2,31 @@ from datetime import datetime
 import pytz
 
 def lines_to_event(lines):
+    # Unescape double escapes
+    lines = [line.replace('\\', '') for line in lines]
+    # Transform lines to event
     event = {}
+    key = None
     for line in lines:
-        if ':' in line:
+        if not line.startswith(' '):
             key, value = line.split(':', 1)
-            value = value.replace('\\', '')
-            try:
-                # Example datetime 20160324T164512Z
-                value = datetime.strptime(value, '%Y%m%dT%H%M%SZ').replace(tzinfo=pytz.UTC)
-            except:
-                pass
             event[key] = value
+        elif key is not None:
+            event[key] += line[1:]
+    # Convert values to datetimes where possible
+    # Example datetime: 20160324T164512Z
+    for key, value in event.items():
+        try:
+            event[key] = datetime.strptime(value, '%Y%m%dT%H%M%SZ').replace(tzinfo=pytz.UTC)
+        except:
+            pass
     return event
 
 def parse(ical):
-    iterator = iter(ical.split('\r\n'))
+    # Normalize linebreaks and split ical into list of lines
+    ical = ical.replace('\r\n', '\n')
+    iterator = iter(ical.split('\n'))
+    # Break lines up into a list of events
     events = []
     while True:
         try:
