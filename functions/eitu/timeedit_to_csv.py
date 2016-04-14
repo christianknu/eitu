17c77327cfc32e5176e791a7bac35ed19ad98bf4
Bjørn Friese <bearfrieze@gmail.com>
2016-04-14 16:53:35 +0200

    Add bookings and correct timezone

--- functions/eitu/timeedit_to_csv.py
+++ functions/eitu/timeedit_to_csv.py
@@ -1,13 +1,27 @@
-import requests
-import csv
+import csv, re
 from datetime import datetime
+import requests
 import ics_parser
+import pytz
 
 URL_STUDY_ACTIVITIES = 'https://dk.timeedit.net/web/itu/db1/public/ri6Q7Z6QQw0Z5gQ9f50on7Xx5YY00ZQ1ZYQycZw.ics'
 URL_ACTIVITIES = 'https://dk.timeedit.net/web/itu/db1/public/ri6g7058yYQZXxQ5oQgZZ0vZ56Y1Q0f5c0nZQwYQ.ics'
+TZ = pytz.timezone('Europe/Copenhagen')
 
 def fetch_and_parse(url):
     return ics_parser.parse(requests.get(url).text)
+
+def write(things, path):
+    with open(path, 'w') as csvfile:
+        fieldnames = set()
+        for t in things: fieldnames = fieldnames | set(t.keys())
+        writer = csv.DictWriter(csvfile, fieldnames=list(fieldnames))
+        writer.writeheader()
+        for t in things:
+            for key, value in t.items():
+                if isinstance(value, datetime):
+                    t[key] = value.astimezone(TZ).isoformat()
+            writer.writerow(t)
 
 # Fetch and parse iCalendar events
 study_activities = fetch_and_parse(URL_STUDY_ACTIVITIES)
@@ -18,13 +32,18 @@ events = study_activities + activities
 events = {e['UID']: e for e in events}.values()
 events = sorted(events, key=lambda e: e['DTSTART'])
 
-# Write csv
-with open('timeedit.csv', 'w') as csvfile:
-    fieldnames = set()
-    for e in events: fieldnames = fieldnames | set(e.keys())
-    writer = csv.DictWriter(csvfile, fieldnames=sorted(list(fieldnames)))
-    writer.writeheader()
-    for e in events:
-        for key, value in e.items():
-            if isinstance(value, datetime): e[key] = value.isoformat()
-        writer.writerow(e)
+# Generate bookings
+bookings = []
+for e in events:
+    for room in e['LOCATION'].split(', '):
+        bookings.append({
+            'room': re.sub(r'^Room: ', '', room),
+            'start': e['DTSTART'],
+            'end': e['DTEND'],
+        })
+
+# Write CSV files
+print(len(events))
+write(events, 'events.csv')
+print(len(bookings))
+write(bookings, 'bookings.csv')
