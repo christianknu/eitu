35c44f0f585d11dea632e509b9eec20d4697dc9d
Bjørn Friese <bearfrieze@gmail.com>
2016-04-14 15:28:54 +0200

    Sort events by start and iso format datetimes

--- functions/eitu/timeedit_to_csv.py
+++ functions/eitu/timeedit_to_csv.py
@@ -1,5 +1,6 @@
 import requests
 import csv
+from datetime import datetime
 import ics_parser
 
 URL_STUDY_ACTIVITIES = 'https://dk.timeedit.net/web/itu/db1/public/ri6Q7Z6QQw0Z5gQ9f50on7Xx5YY00ZQ1ZYQycZw.ics'
@@ -13,8 +14,9 @@ study_activities = fetch_and_parse(URL_STUDY_ACTIVITIES)
 activities = fetch_and_parse(URL_ACTIVITIES)
 events = study_activities + activities
 
-# Remove duplicate events
+# Remove duplicates and sort
 events = {e['UID']: e for e in events}.values()
+events = sorted(events, key=lambda e: e['DTSTART'])
 
 # Write csv
 with open('timeedit.csv', 'w') as csvfile:
@@ -22,4 +24,7 @@ with open('timeedit.csv', 'w') as csvfile:
     for e in events: fieldnames = fieldnames | set(e.keys())
     writer = csv.DictWriter(csvfile, fieldnames=sorted(list(fieldnames)))
     writer.writeheader()
-    for e in events: writer.writerow(e)
+    for e in events:
+        for key, value in e.items():
+            if isinstance(value, datetime): e[key] = value.isoformat()
+        writer.writerow(e)
